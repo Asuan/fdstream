ed5e8ac45938160eaf33895d4b99d59ccf1d854e
Asuan <sngasuan@gmail.com>
2017-12-06 16:35:12 +0300

    add restore fpr asunc

--- async_client.go
+++ async_client.go
@@ -24,116 +24,113 @@ type AsyncWriter interface {
 	WriteBytes(code byte, name string, payload []byte) error
 }
 
-type AsyncHandler interface {
-	AsyncWriter
-	IsAlive() bool
-	Read() *Message
-}
-
 type AsyncClient struct {
 	OutputStream   io.Writer
 	InputStream    io.ReadCloser
 	toSendMessageQ chan *Message //Queue to send to remote
 	toReadMessageQ chan *Message //Queue to read message
-	killer         sync.Once
+	killer         *sync.Once
+	restorer       *sync.Once
 	kill           chan bool
 	alive          atomic.Value
 }
 
 //NewAsyncHandler create async handler
-func NewAsyncHandler(outcome io.Writer, income io.ReadCloser) (AsyncHandler, error) {
+func NewAsyncHandler(outcome io.Writer, income io.ReadCloser) (*AsyncClient, error) {
 	c := &AsyncClient{
 		OutputStream:   outcome,
 		InputStream:    income,
 		toSendMessageQ: make(chan *Message, defaultQSize),
 		toReadMessageQ: make(chan *Message, defaultQSize),
-		kill:           make(chan bool),
+		kill:           make(chan bool, 1),
+		killer:         new(sync.Once),
+		restorer:       new(sync.Once),
 	}
 	c.alive.Store(true)
 
-	//Read message by message from input reader
-	workerReader := func(c *AsyncClient, outcome chan<- *Message) {
-		var (
-			err         error
-			n           int
-			lenB        int //full length of body without header
-			lenP        uint16
-			cursor      uint16
-			id          uint32
-			code        byte
-			eof         bool
-			header      = make([]byte, messageHeaderSize, messageHeaderSize)
-			messageBody = make([]byte, MaxMessageSize, MaxMessageSize)
-		)
+	go c.workerReader(c.toReadMessageQ)
+	go c.workerWriter(c.toSendMessageQ)
+	return c, nil
+}
 
-		for !eof {
-			n, err = c.InputStream.Read(header)
-			if err != nil {
-				break //If we get error so looks like no way to continue
-			}
-			if n == 0 { //Skip empty read
-				continue
-			}
+//Read message by message from input reader
+func (c *AsyncClient) workerReader(outcome chan<- *Message) {
+	var (
+		err         error
+		n           int
+		lenB        int //full length of body without header
+		lenP        uint16
+		cursor      uint16
+		id          uint32
+		code        byte
+		eof         bool
+		header      = make([]byte, messageHeaderSize, messageHeaderSize)
+		messageBody = make([]byte, MaxMessageSize, MaxMessageSize)
+	)
 
-			if n != messageHeaderSize { //Wrong message header read looks broken state
+	for !eof {
+		n, err = c.InputStream.Read(header)
+		if err != nil {
+			break //If we get error so looks like no way to continue
+		}
+		if n == 0 { //Skip empty read
+			continue
+		}
+
+		if n != messageHeaderSize { //Wrong message header read looks broken state
+			break
+		}
+		//TODO optimize reading
+		code, id, cursor, lenP = unmarshalHeader(header)
+		m := &Message{
+			Code: code,
+			Id:   id,
+
+			//This is so slow operation but we should copy data from messageBody buffer
+			Payload: make([]byte, lenP, lenP),
+		}
+		messageBody = messageBody[:(cursor + lenP)]
+
+		lenB, err = c.InputStream.Read(messageBody)
+		if err != nil {
+			//try read last message if EOF appear
+			if err != io.EOF || lenB != len(messageBody) {
 				break
 			}
-			//TODO optimize reading
-			code, id, cursor, lenP = unmarshalHeader(header)
-			m := &Message{
-				Code: code,
-				Id:   id,
-
-				//This is so slow operation but we should copy data from messageBody buffer
-				Payload: make([]byte, lenP, lenP),
-			}
-			messageBody = messageBody[:(cursor + lenP)]
-
-			lenB, err = c.InputStream.Read(messageBody)
-			if err != nil {
-				//try read last message if EOF appear
-				if err != io.EOF || lenB != len(messageBody) {
-					break
-				}
-				eof = true
-			}
-
-			if cursor > 0 {
-				m.Name = string(messageBody[0:cursor])
-			}
-
-			if lenP > 0 {
-				copy(m.Payload, messageBody[cursor:cursor+lenP])
-			}
-
-			outcome <- m
+			eof = true
 		}
-		c.shutdown()
-	}
 
-	//Write message by message to output reader from chan
-	workerWriter := func(c *AsyncClient, income <-chan *Message) {
-		var (
-			err error
-			m   *Message
-		)
-	mainLoop:
-		for {
-			select {
-			case <-c.kill:
+		if cursor > 0 {
+			m.Name = string(messageBody[0:cursor])
+		}
+
+		if lenP > 0 {
+			copy(m.Payload, messageBody[cursor:cursor+lenP])
+		}
+
+		outcome <- m
+	}
+	c.Shutdown()
+}
+
+//Write message by message to output reader from chan it can be run in multiple instances
+func (c *AsyncClient) workerWriter(income <-chan *Message) {
+	var (
+		err error
+		m   *Message
+	)
+mainLoop:
+	for {
+		select {
+		case <-c.kill:
+			break mainLoop
+		case m = <-income:
+			if _, err = m.WriteTo(c.OutputStream); err != nil {
 				break mainLoop
-			case m = <-income:
-				if _, err = m.WriteTo(c.OutputStream); err != nil {
-					break mainLoop
-				}
 			}
 		}
-		c.shutdown()
 	}
-
-	go workerReader(c, c.toReadMessageQ)
-	go workerWriter(c, c.toSendMessageQ)
-	return c, nil
+	c.Shutdown()
 }
 
 //Write will write message to destination
@@ -169,15 +166,14 @@ func (c *AsyncClient) Read() *Message {
 	return <-c.toReadMessageQ
 }
 
-//Shutdown close  readbut save un-readed or un-writhed data.
-func (c *AsyncClient) shutdown() {
+//Shutdown close  read but save un-readed or un-writhed data.
+func (c *AsyncClient) Shutdown() {
 	//DO not close chans need grace safe in-progress messages
 	c.killer.Do(func() {
 		close(c.kill)         //It should stop writer
 		c.InputStream.Close() //We should notify all 3d writes about trouble.
-		//TODO save unprocessed data
-		//TODO kill instance to clean data in q
 
+		c.restorer = new(sync.Once) //To keep way to restore
 	})
 	c.alive.Store(false)
 }
@@ -185,4 +181,20 @@ func (c *AsyncClient) shutdown() {
 //IsAlive notify about state of async client
 func (c *AsyncClient) IsAlive() bool {
 	return c.alive.Load().(bool)
+}
+
+//Restore restore after client after killing
+func (c *AsyncClient) Restore(outcome io.Writer, income io.ReadCloser) {
+	if !c.IsAlive() {
+		c.restorer.Do(func() {
+			c.killer = new(sync.Once)
+			c.InputStream = income
+			c.OutputStream = outcome
+			c.kill = make(chan bool, 1)
+			go c.workerReader(c.toReadMessageQ)
+			go c.workerWriter(c.toSendMessageQ)
+
+			c.alive.Store(true)
+		})
+	}
 }
